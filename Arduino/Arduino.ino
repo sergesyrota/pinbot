eda7b5515874055fdd081dcbbc7a6ffb5cdd4f39
Sergey Syrota <serge.syrota@gmail.com>
2016-11-28 22:23:27 -0600

    Adding serial communication to Arduino. That module is complete now.

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -9,9 +9,31 @@ void setup() {
 
   pinMode(A_OUTPUT, OUTPUT);
   pinMode(B_OUTPUT, OUTPUT);
+
+  Serial.begin(115200);
 }
 
 void loop() {
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+    switch (c) {
+      case 'a':
+        engage(A_OUTPUT, SHORT_ENGAGE_MS);
+        break;
+      case 'A':
+        engage(A_OUTPUT, LONG_ENGAGE_MS);
+        break;
+      case 'b':
+        engage(B_OUTPUT, SHORT_ENGAGE_MS);
+        break;
+      case 'B':
+        engage(B_OUTPUT, LONG_ENGAGE_MS);
+        break;
+      default:
+        // Do nothing, just ignore
+    }
+  }
+
   bool button;
   // Checking A button
   button = digitalRead(A_INPUT);
